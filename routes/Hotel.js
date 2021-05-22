6ffde21d9a1ceb0bcde9023ff2159e6b28f7f29e
Jun Park <jhpp114node@gmail.com>
2021-05-22 12:26:49 -0700

    added controller and apply it to route

--- routes/Hotel.js
+++ routes/Hotel.js
@@ -1,33 +1,8 @@
 // Router for Hotel
 const express = require("express");
-const fetch = require("node-fetch");
-require("dotenv").config();
 const router = express.Router();
+const hotelController = require("../controllers/hotelController");
 
-router.get("/search/:city", async (req, res) => {
-  //controller goes here
-  const cityname = req.params.city;
-  const HOTEL_API_URL = `http://engine.hotellook.com/api/v2/lookup.json?query=${cityname}&lang=en&lookFor=both&limit=5&token=${process.env.HOTEL_API_KEY}`;
-  try {
-    const fetchHotelData = await fetch(HOTEL_API_URL);
-    const hotelData = await fetchHotelData.json();
-    if (hotelData.status !== "ok" || hotelData.results.hotels.length === 0) {
-      throw "API Status is bad";
-    }
-    // get correct country
-    const fullCountryName = hotelData.results.locations[0].countryName;
-    const filteredData = hotelData.results.hotels.filter((eachHotel) => {
-      return eachHotel.locationName.indexOf(fullCountryName) >= 0;
-    });
-    console.log(filteredData);
-    const renderDataObj = {
-      searchedCity: cityname,
-      filteredData: filteredData,
-    };
-    res.render("search/search.ejs", { renderDataObj: renderDataObj });
-  } catch (error) {
-    console.error(error);
-  }
-});
+router.get("/search/:city", hotelController.hotel_search_index);
 
 module.exports = router;
